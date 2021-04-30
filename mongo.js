c8ebb19ed5f63ecc445137b8dcf316caa2c6342c
gillianroeca <78461792+gillianroeca@users.noreply.github.com>
2021-04-29 21:23:05 -0400

    Add files via upload

--- mongo.js
+++ mongo.js
@@ -2,19 +2,18 @@ const MongoClient = require('mongodb').MongoClient;
 var readline = require('readline');
 var fs = require('fs');
 const url = "mongodb+srv://cate1344:Cad94010@cluster0.tbm9v.mongodb.net/Recipe_Book?retryWrites=true&w=majority";  // connection string goes here
-var port = process.env.PORT || 3000;
 
   MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
   if(err) { return console.log(err); return;}
+
   
-  
-  
-  
+
+
   var dbo = db.db("Recipe_Book");
   var dnl = dbo.collection('Did_Not_Like');
   var fav = dbo.collection('Favorites');
   var wtt = dbo.collection('Want_To_Try');
-  
+
   file.on('line', function (line) {
       var parts = line.split(',');
       console.log(parts[0] + " " + parts[1]);
@@ -30,8 +29,8 @@ var port = process.env.PORT || 3000;
       //console.log(line);
   });
 
-	
+
 	console.log("Success!");
 	//db.close(); NEED TO CLOSE LATER?!?!!
- 
+
 });
--- recipe.js
+++ recipe.js
@@ -6,20 +6,19 @@ var readline = require('readline');
 const url = "mongodb+srv://cate1344:Cad94010@cluster0.tbm9v.mongodb.net/assignment_14?retryWrites=true&w=majority";  // connection string goes here
 var port = process.env.PORT || 3000;
 
-
 http.createServer(function(req, res) {
-    
+
     //writing the html form onto the webpage
     if (req.url == "/")
     {
-        file = "form-1.html";
+        file = "form.html";
         fs.readFile(file, function(err, text) {
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(text);
             res.end();
         });
-    } 
-    
+    }
+
     else if (req.url == "/recipes")
     {
             file = "recipe.html";
@@ -28,9 +27,9 @@ http.createServer(function(req, res) {
                 res.write(text);
                 res.end();
             });
-        
-            
-        
+
+
+
     }
-    
+
 }).listen(port);
