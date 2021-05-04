69cdde017acae4d1d9069a91729ae9bd9ac3f34f
gillianroeca <78461792+gillianroeca@users.noreply.github.com>
2021-05-04 15:50:57 -0400

    Add files via upload

--- recipe.js
+++ recipe.js
@@ -4,8 +4,8 @@ var qs = require('querystring');
 const MongoClient = require('mongodb').MongoClient;
 var readline = require('readline');
 const url = "mongodb+srv://cate1344:Cad94010@cluster0.tbm9v.mongodb.net/Recipe_Book?retryWrites=true&w=majority";  // connection string goes here
-var port = process.env.PORT || 3000;
-//var port = 8080;
+//var port = process.env.PORT || 3000;
+var port = 8080;
 
 http.createServer(function(req, res) {
     //writing the html form onto the webpage
@@ -61,48 +61,50 @@ http.createServer(function(req, res) {
 
     else if (req.url == "/add")
     {
-        pdata = "";
-        req.on('data', data => {
-            pdata += data.toString();
-        });
+      pdata = "";
+      req.on('data', data => {
+          pdata += data.toString();
+      });
 
-        req.on('end', () => {
-            var boxNum = pdata[3];
-            pdata = qs.parse(pdata);
-            var coll = "";
-            if (pdata['choose' + boxNum] == 'Favorites')
-            {
-                coll = "Favorites";
-                console.log(coll);
-            }
-            else if (pdata['choose' + boxNum] == 'Did Not Like')
-            {
-                coll = "Did_Not_Like";
-                console.log(coll);
-            }
-            else if (pdata['choose' + boxNum] == 'Want To Try')
-            {
-                coll = "Want_To_Try";
-                console.log(coll);
-            }
-            else
-            {
-                console.log("here");
-            }
+      req.on('end', () => {
+          var boxNum = pdata[3];
+          pdata = qs.parse(pdata);
+          var coll = "";
+          if (pdata['choose' + boxNum] == 'Favorites')
+          {
+              coll = "Favorites";
+              console.log(coll);
+          }
+          else if (pdata['choose' + boxNum] == 'Did Not Like')
+          {
+              coll = "Did_Not_Like";
+              console.log(coll);
+          }
+          else if (pdata['choose' + boxNum] == 'Want To Try')
+          {
+              coll = "Want_To_Try";
+              console.log(coll);
+          }
+          else
+          {
+              console.log("here");
+          }
 
 
-            //var coll = pdata['choose' + boxNum]; ------
-            var pname = pdata['name' + boxNum];
-            var purl = pdata['url' + boxNum];
-            var pimage = pdata['image' + boxNum];
+          //var coll = pdata['choose' + boxNum]; ------
+          var pname = pdata['name' + boxNum];
+          var purl = pdata['url' + boxNum];
+          var pimage = pdata['image' + boxNum];
 
             insert_mongo(coll, pname, purl, pimage);
         });
+
         file = "recipe.html";
 
         fs.readFile(file, function(err, text) {
-            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.writeHead(200, {'Content-Type': 'text'});
             res.write(text);
+            res.url = "/";
             res.end();
         });
 
@@ -118,14 +120,39 @@ http.createServer(function(req, res) {
        req.on('end', () => {
            var boxNum = pdata[3];
            pdata = qs.parse(pdata);
-           var coll = pdata['choose' + boxNum];
+           var coll = "";
+           var name = "";
+          if (pdata['choose' + boxNum] == 'Favorites')
+          {
+              coll = "Favorites";
+              name = "Favorites";
+              console.log(coll);
+          }
+          else if (pdata['choose' + boxNum] == 'Did_Not_Like')
+          {
+              coll = "Did_Not_Like";
+              name = "Did Not Like";
+              console.log(coll);
+          }
+          else if (pdata['choose' + boxNum] == 'Want_To_Try')
+          {
+              coll = "Want_To_Try";
+              name = "Want To Try";
+              console.log(coll);
+          }
+          else
+          {
+              console.log("here");
+          }
+
+           // var coll = pdata['choose' + boxNum];
            var pname = pdata['name' + boxNum];
            var purl = pdata['url' + boxNum];
            var pimage = pdata['image' + boxNum];
 
            delete_mongo(coll, pname, purl, pimage,
                function() {
-                   show_collection(coll, coll, function(output) {
+                   show_collection(coll, name, function(output) {
                        res.writeHead(200, {'Content-Type': 'text/html'});
                        res.write(output);
                        res.end();
@@ -135,8 +162,8 @@ http.createServer(function(req, res) {
        });
    }
 
-}).listen(port);
-//}).listen(8080);
+//}).listen(port);
+}).listen(8080);
 
 function insert_mongo(coll, pname, purl, pimage) {
     MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
@@ -154,6 +181,7 @@ function insert_mongo(coll, pname, purl, pimage) {
                     return;
                 }
                 console.log(newData);
+                // alert(newData["name"] + " added to " + collection);
             });
 
         //db.close(); --> must close in node.js with ^C
@@ -194,12 +222,14 @@ function show_collection(coll, header_name, callback) {
       collection.find({}).toArray(function(err, result) {
           if (err) throw err;
 
+          output += "<head><style>body{background-image: url('https://photos.smugmug.com/photos/i-pM4SXgb/0/d6b50170/M/i-pM4SXgb-M.jpg');";
+          output += "background-repeat: no-repeat;background-attachment: fixed;background-size: cover;}</style></head>";
           output += "<h1 style = 'background-color: #f74a4a; text-align: center;";
           output += "padding: 30px; top: 0; left:0; width: 100%; z-index: 2;";
           output += "text-shadow: -1px 1px 0 #FFFFFF, 1px 1px 0 #FFFFFF, 1px -1px 0 #FFFFFF, -1px -1px 0 #FFFFFF;'>";
           output += header_name + "</h1><br>";
-          //output += "<form method='post' action='http://localhost:8080/'>";
-          output += "<form method='post' action='https://codebloodedfinal.herokuapp.com/'>";
+          output += "<form method='post' action='http://localhost:8080/'>";
+          //output += "<form method='post' action='https://codebloodedfinal.herokuapp.com/'>";
           output += "<input style = 'text-align:center; background-color:black; left:3vw;";
           output += "background-image: linear-gradient(white, #FF853B); background-size: 30px 100%, 100% 100%; ";
           output += "width:20vw; height:50px; top:20px; line-height:20px; color:black; margin: 5px 8px;";
@@ -212,10 +242,10 @@ function show_collection(coll, header_name, callback) {
           for (i=0; i<result.length; i++)
           {
               output += "<div class='recipe_block' style='margin: 25px; border: 3px solid #000; float: left; width: 300px; height: 340px; background-color: #c3ebe7;'>"
-              output += "<a href='" + result[i]["url"] + "'>" + result[i]["name"] + "</a><br>";
+              output += "<a href='" + result[i]["url"] + "' target='_blank'>" + result[i]["name"] + "</a><br>";
               output += "<img src='" + result[i]["image"] + "'><br>";
-              //output += "<form method='post' action='http://localhost:8080/delete'>";
-              output += "<form method='post' action='https://codebloodedfinal.herokuapp.com/delete'>";
+              output += "<form method='post' action='http://localhost:8080/delete'>";
+              //output += "<form method='post' action='https://codebloodedfinal.herokuapp.com/delete'>";
               output += "<input type='submit' name='box"+i+"' value='Delete'>"
               output += "<input type='text' id='collection"+i+"' name='choose"+i+"' value='"+coll+"' style='display:none'/>";
               output += "<input type='text' id='name"+i+"' name='name"+i+"' value='"+result[i]["name"]+"' style='display:none'/>";
